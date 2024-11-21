8bb8795d57e43f1c0423281ea1b6064e02d06b01
Jooaomarcelo <joaomdsb@hotmail.com>
2024-11-21 19:50:12 -0300

    Contador de pontos terminado com função de resetar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,42 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        debugPaintSizeEnabled = false;
+        return child!;
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Contador de Pontos'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +36,143 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _player1 = 0;
+  int _player2 = 0;
+  String _winner = '';
 
-  void _incrementCounter() {
+  final List<int> _maxPointsList = [5, 10, 15, 20, 25];
+  int? _maxPoints = 5;
+
+  // Funtion to change the max points
+  void _onChange(int? value) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _maxPoints = value;
+    });
+  }
+  // End of funtion to change the max points
+
+  // Functions to increment the score of each player
+  void _incrementPlayer1() {
+    setState(() {
+      if (_winner.isEmpty) {
+        _player1++;
+        if (_player1 == _maxPoints) {
+          _winner = 'Player 1';
+        }
+      }
     });
   }
 
+  void _incrementPlayer2() {
+    setState(() {
+      if (_winner.isEmpty) {
+        _player2++;
+        if (_player2 == _maxPoints) {
+          _winner = 'Player 2';
+        }
+      }
+    });
+  }
+  // End of functions to increment the score of each player
+
+  // Funtion to reset the scores
+  void _resetGame() {
+    setState(() {
+      _player1 = 0;
+      _player2 = 0;
+      _winner = '';
+    });
+  }
+  // End of funtion to reset the scores
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (_winner.isEmpty) ...[
+                Text(
+                  'Pontuação:',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  '$_player1 x $_player2',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FloatingActionButton(
+                      onPressed: _incrementPlayer1,
+                      backgroundColor: Colors.green,
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    FloatingActionButton(
+                      onPressed: _incrementPlayer2,
+                      backgroundColor: Colors.red,
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: 150,
+                  child: DropdownButtonFormField(
+                    value: _maxPoints,
+                    items: _maxPointsList.map((int item) {
+                      return DropdownMenuItem<int>(
+                        value: item,
+                        child: Text(item.toString()),
+                      );
+                    }).toList(),
+                    onChanged: _onChange,
+                  ),
+                ),
+              ] // isEmpty
+              else ...[
+                Text(
+                  '$_winner venceu!',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                    width: 100,
+                    child: FloatingActionButton(
+                      onPressed: _resetGame,
+                      backgroundColor: Colors.lightBlue,
+                      child: const Text('Recomeçar'),
+                    )),
+              ]
+            ],
+          ),
+        ),
+        floatingActionButton: SizedBox(
+          child: FloatingActionButton(
+            onPressed: _resetGame,
+            tooltip: 'Resetar',
+            backgroundColor: Colors.black,
+            child: const Icon(
+              Icons.restore_rounded,
+              color: Colors.amber,
+            ),
+          ),
+        ));
   }
 }
